12d463705c0dc0bf69b48d59117d28d70d7594a2
Raphael8119-maker <latelierdececilearthes@gmail.com>
2025-04-27 16:49:30 +0200

    Update ArbreCompetence.py

--- ArbreCompetence.py
+++ ArbreCompetence.py
@@ -1,11 +1,33 @@
 import arcade
 
+WINDOW = arcade.Window(title= "Compétence-Click_Lab")
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
-WINDOW_TITLE = "Compétence-Click_Lab"
-allCompetence = {"Competence1":["competence2.1","competence2.2"],"competence2.1":["competence3.1","competence3.2"]}
-competenceDéblocable = []
+#allCompetence = {"Competence1":["competence2.1","competence2.2"],"competence2.1":["competence3.1","competence3.2"]}
+#competenceDéblocable = []
 
-class Main(arcade):
+class GameView(arcade.View):
     def __init__(self): 
-        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+        super().__init__()
+        
+        self.directions = {'left':False,'right':False,'up':False,'down':False,}
+        
+        self.window.set_mouse_visible(True)
+        
+        self.background_color = arcade.csscolor.BLACK
+
+        
+    def setup(self):
+        pass
+
+    def on_draw(self):
+         self.clear() 
+         
+def main():
+    """Main function"""
+    game = GameView()
+    WINDOW.show_view(game)
+    arcade.run()
+    
+if __name__ == "__main__":
+    main()
