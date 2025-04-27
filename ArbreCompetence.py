5e72660e90be5965f9d94eaab682451f87f4b182
Raphael8119-maker <latelierdececilearthes@gmail.com>
2025-04-27 09:59:18 +0200

    Update ArbreCompetence.py

--- ArbreCompetence.py
+++ ArbreCompetence.py
@@ -1,5 +1,11 @@
 import arcade
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+WINDOW_TITLE = "Compétence-Click_Lab"
 allCompetence = {"Competence1":["competence2.1","competence2.2"],"competence2.1":["competence3.1","competence3.2"]}
 competenceDéblocable = []
 
-class Main(arcade.gui.UIView):
+class Main(arcade):
+    def __init__(self): 
+        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
