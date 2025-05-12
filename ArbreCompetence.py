b34ca168bc3d4e3d15db1643e4c043fca9fc8e14
Raphael8119-maker <latelierdececilearthes@gmail.com>
2025-05-12 17:13:25 +0200

    Create ArbreCompetence.py

---
+++ ArbreCompetence.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+# Création de la fenêtre principale (Main Window)
+fenetre = Tk()
+fenetre.title('ArbreCompetence - ClickLab')
+fenetre.geometry('1280x720')
+
+fenetre.mainloop()
