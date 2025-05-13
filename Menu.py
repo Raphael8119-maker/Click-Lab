060413717129df754a643d825976db72a7d356ec
Raphael8119-maker <latelierdececilearthes@gmail.com>
2025-05-13 17:03:19 +0200

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -10,10 +10,8 @@ fn.geometry("1280x720")
 fn.resizable(width=False, height=False)
 fn.configure(bg="light blue")
 
+my_font = tkFont.Font(size = 20)
 
-
-
-
-
+Button(fn, text="Jouer", font=my_font, command=lambda: Main.lancer_main()).place(x=0, y=0)
 
 fn.mainloop()
