ff2e5f28c1af0e44a1198f3a1fc86837df2f5f2d
Raphael8119-maker <latelierdececilearthes@gmail.com>
2025-05-13 16:53:09 +0200

    Create Menu.py

---
+++ Menu.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import tkinter.font as tkFont
+import Rng
+import Main
+
+fn = Tk()
+fn.title("Main - ClickLab")
+fn.geometry("1280x720")
+fn.resizable(width=False, height=False)
+fn.configure(bg="light blue")
+
+
+
+
+
+
+
+fn.mainloop()
