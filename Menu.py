5b96723b1246eb022e7fc9d1aa15dee6b1ebd137
Raphael8119-maker <latelierdececilearthes@gmail.com>
2025-05-14 19:20:00 +0200

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -3,20 +3,35 @@ from PIL import Image, ImageTk
 import tkinter.font as tkFont
 import Main
 
-recherche = 0
+def menu(recherche_initial = 0):
+    """
+    
+    Lance l'interface graphique du menu.
+    
+    """
+    
+    recherche = recherche_initial
 
-fn = Tk()
-fn.title("Main - ClickLab")
-fn.geometry("1280x720")
-fn.resizable(width=False, height=False)
-fn.configure(bg="light blue")
+    fn = Tk()
+    fn.title("Main - ClickLab")
+    fn.geometry("1280x720")
+    fn.resizable(width=False, height=False)
+    fn.configure(bg="light blue")
 
-my_font = tkFont.Font(size = 20)
+    my_font = tkFont.Font(size = 20)
 
-def jouer():
-    fn.destroy()
-    Main.lancer_main(recherche)
+    def jouer():
+        """
+        
+        Ferme la fenêtre actuelle et ouvre l'interface du jeu via le module Main.
+        
+        """
+        fn.destroy()
+        Main.lancer_main(recherche)
 
-Button(fn, text="Jouer", font=my_font, command=jouer).place(x=0, y=0)
+    Button(fn, text="Jouer", font=my_font, command=jouer).place(x=0, y=0)
 
-fn.mainloop()
+    fn.mainloop()
+    
+if __name__ == "__main__":
+    menu()
