2163146ca39ca4b26b6ceafe32913c35eeb3db74
Raphael8119-maker <latelierdececilearthes@gmail.com>
2025-05-16 21:16:47 +0200

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -12,6 +12,7 @@ def menu(recherche_initial = 0,fn = None):
     """
     recherche = recherche_initial
 
+    #Permet de savoir si une fenetre et dejà ouverte, si oui on nettoie l'ancienne et si non on en créer un nouvelle
     if fn is None :
         fn = tk.Tk()
         fn_bonne = True
@@ -20,11 +21,13 @@ def menu(recherche_initial = 0,fn = None):
             widget.destroy()
         fn_bonne = False
 
+    # Changement de la fenêtre de jeu
     fn.title("Menu - ClickLab")
     fn.geometry("1280x720")
     fn.resizable(width=False, height=False)
     fn.configure(bg="light blue")
 
+    # Taille des boutons
     my_font = tkFont.Font(size = 20)
 
     def jouer():
@@ -36,9 +39,14 @@ def menu(recherche_initial = 0,fn = None):
         for widget in fn.winfo_children():
             widget.destroy()
         Main.lancer_main(recherche,fn)
-
+        
+    # Bouton pour aller dans le jeu
     Button(fn, text="Jouer", font=my_font, command=jouer).place(x=580, y=380)
-    texteLabel = Label(fn, text = "Click Lab", bg= "light blue", font=("Courier", 50)).place (x=450, y=120)
+    
+    # Affichage du titre
+    Label(fn, text = "Click Lab", bg= "light blue", font=("Courier", 50)).place (x=450, y=120)
+    
+    # Bouton pour quitter le jeu
     Button(fn, text="Quitter", font=my_font, command=fn.destroy).place(x=573, y=500)
 
     if fn_bonne :
