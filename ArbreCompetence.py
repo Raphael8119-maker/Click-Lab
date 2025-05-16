b6cfe613dc086c4ae69702ffc903ac5672cb9dd3
Raphael8119-maker <latelierdececilearthes@gmail.com>
2025-05-16 19:23:04 +0200

    Update ArbreCompetence.py

--- ArbreCompetence.py
+++ ArbreCompetence.py
@@ -1,11 +1,8 @@
 from tkinter import *
 from PIL import Image, ImageTk
-
-fenetre = Tk()
-fenetre.title("Arbre des Compétences")
-fenetre.geometry("1280x800")
-fenetre.resizable(False, False)
-fenetre.configure(bg="lightblue")
+import Rng
+import Main
+import tkinter.font as tkFont
 
 competences = {
     "Compétence 1": 5,
@@ -20,44 +17,66 @@ competences = {
     "Compétence 10": 50
 }
 
-nb_recherche = IntVar()
-nb_recherche.set(150)
-
-
-# Interface graphique
-texte = StringVar()
-texte.set("Arbre des Compétences")
-Label(fenetre, textvariable=texte, font=("Arial", 20), fg='black').place(x=500, y=10)
-
-image = Image.open("fiole.png")
-image_logo = ImageTk.PhotoImage(image)
-Label(fenetre, image=image_logo, bg="lightblue").place(x=560, y=50)
-
-Label(fenetre, text="Nombre de recherche :", font=("Arial", 14), bg="lightblue").place(x=50, y=200)
-valeur_recherche = StringVar()
-valeur_recherche.set(str(nb_recherche.get()))
-Label(fenetre, textvariable=valeur_recherche, font=("Arial", 14), bg="lightblue").place(x=250, y=200)
-
-texte_info = StringVar()
-texte_info.set("")
-Label(fenetre, textvariable=texte_info, font=("Arial", 14), fg="blue", bg="lightblue").place(x=50, y=240)
-
-# fonction pour acheter les compétences
-def acheter_competence(nom, cout):
-    actuel = nb_recherche.get()
-    if actuel >= cout:
-        nb_recherche.set(actuel - cout)
-        valeur_recherche.set(str(nb_recherche.get()))
-        texte_info.set(nom + " débloquée !")
-        del competences(nom)
+def lancer_arbretechno_interface(recherche_initial, competencesLootBoxRare, lootboxes, fn) :
+    
+    recherche = recherche_initial
+    
+    #Permet de savoir si une fenetre et dejà ouverte, si oui on nettoie l'ancienne et si non on en créer un nouvelle
+    if fn is None:
+        fn = Tk()
+        fn_bonne = True
     else:
-        texte_info.set("Pas assez de recherche pour " + nom)
+        fn_bonne = False
+        for widget in fn.winfo_children():
+            widget.destroy()
+    
+    fn.title("Arbre des Compétences")
+    fn.geometry("1280x720")
+    fn.resizable(False, False)
+    fn.configure(bg="lightblue")
+    
+    # Taille des boutons
+    my_font = tkFont.Font(size=20)
+        
+    # Interface graphique
+    texte = StringVar()
+    texte.set("Arbre des Compétences")
+    Label(fn, textvariable=texte, font=("Arial", 20), fg='black').place(x=500, y=10)
 
-y = 300
-# boucle pour pouvoir créer les boutons de compétences automatiquement
-for nom, cout in competences.items():
-    texte_bouton = nom + " (" + str(cout) + ")"
-    Button(fenetre, text=texte_bouton, width=30, command=lambda n=nom, c=cout: acheter_competence(n, c)).place(x=50, y=y)
-    y += 50
+    Label(fn, text="Nombre de recherche :", font=("Arial", 14), bg="lightblue").place(x=50, y=200)
+    valeur_recherche = StringVar()
+    valeur_recherche.set(str(recherche))
+    Label(fn, textvariable=valeur_recherche, font=("Arial", 14), bg="lightblue").place(x=250, y=200)
 
-fenetre.mainloop()
+    texte_info = StringVar()
+    texte_info.set("")
+    Label(fn, textvariable=texte_info, font=("Arial", 14), fg="blue", bg="lightblue").place(x=50, y=240)
+
+    # fonction pour acheter les compétences
+    def acheter_competence(nom, cout):
+        if recherche >= cout:
+            recherche.set(recherche - cout)
+            valeur_recherche.set(str(recherche))
+            texte_info.set(nom + " débloquée !")
+        else:
+            texte_info.set("Pas assez de recherche pour " + nom)
+
+    # boucle pour pouvoir créer les boutons de compétences automatiquement
+    y = 300
+    for nom, cout in competences.items():
+        texte_bouton = nom + " (" + str(cout) + ")"
+        Button(fn, text=texte_bouton, width=30, command=lambda n=nom, c=cout: acheter_competence(n, c)).place(x=50, y=y)
+        y += 50
+        
+    def retour():
+        """
+        
+        Ouvre l'interface du jeu via le module Main.
+        
+        """
+        Main.lancer_main(recherche, fn) 
+        
+    Button(fn, text="retour", font=my_font, command=retour).place(x=1180, y=650)
+    
+    if fn_bonne :
+        fn.mainloop()
